80799045d0230c648fbf6daa7d5e48de827a0863
ggeorg <georgopoulos.georgios@gmail.com>
2017-06-14 17:25:21 +0300

    XCode project.

---
+++ ios/j2objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  j2objc
+//
+//  Created by George Georgopoulos on 07/06/2017.
+//  Copyright © 2017 VeraCloud Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ios/j2objc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  j2objc
+//
+//  Created by George Georgopoulos on 07/06/2017.
+//  Copyright © 2017 VeraCloud Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
